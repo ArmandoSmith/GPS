b86ad5f6a73b7aa2722f6c04539a976407b09376
Smith <31020266+ArmandoSmith@users.noreply.github.com>
2018-11-27 19:25:45 -0800

    .

--- expendly/lib/register.dart
+++ expendly/lib/register.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'login.dart';
-//import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import 'login.dart';
 
